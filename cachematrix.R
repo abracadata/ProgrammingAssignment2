71f2a824148705e48debeac2e5e3ed90724d2c13
abracadata <linda@abracadata.ca>
2014-11-23 18:27:40 -0700

    more comments added; initial code for makeCacheMatrix added

--- cachematrix.R
+++ cachematrix.R
@@ -4,9 +4,22 @@
 # cache its own inverse.  
 
 ## Write a short comment describing this function
-# This function creates a matrix object and its inverse
+# This function creates a matrix object that contains the matrix and its inverse
+# For reference:
+#  - the syntax for creating a matrix is:
+#      matrix(data = NA, nrow = 1, ncol = 1, byrow = FALSE, dimnames = NULL)
+#  - the syntax for getting the columns or rows in a matrix:
+#      nrow(nameofmatrix)
+#      ncol(nameofmatrix)
+
 makeCacheMatrix <- function(x = matrix()) {
-  print("Homework not finished.")
+# get the rows in matrix "x"
+    rx <- nrow(x)
+# get the cols in matrix "x"
+    cx <- ncol(x)
+# create an inverse matrix "invx" of "x" with inverse rows and columns
+    invx <- matrix(c(0), nrow = cx, ncol = rx)
+# fill the matrix     
 }
 
 
