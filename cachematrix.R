bd7dbe2e575a86626d7529ac6673edea437fb680
abracadata <linda@abracadata.ca>
2014-11-23 17:28:48 -0700

    1st commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,12 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+# This comment is a placeholder until I can think of a better comment
 
 ## Write a short comment describing this function
+# This function does nothing because I didn't finish the homework
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  print("Homework not finished.")
 }
 
 
